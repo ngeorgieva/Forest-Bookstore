0139c9ae66b3862143d377088a60b1d26c13c815
Mihail Mihaylov <mishomihaylov91@gmail.com>
2016-07-31 18:39:07 +0300

    Books.cs Added

---
+++ ForestBookstore/ForestBookstore/Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ForestBookstore.Models.DbContext;
+
+namespace ForestBookstore.Models
+{
+    public class Books
+    {
+        public ICollection<Book> GetNewest(int count = 6)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            var newestBooks = db.Books.OrderBy(book => book.CreatedOn).Take(count).ToList();
+
+            return newestBooks;
+        }
+    }
+}
