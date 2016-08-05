c139ce7da2fd68f6c7c7fb74e9e7721abc0347ff
ngeorgieva <nat.m.georgieva@gmail.com>
2016-08-05 13:12:24 +0300

    Home page redone and book details view reformatted

--- ForestBookstore/ForestBookstore/Controllers/HomeController.cs
+++ ForestBookstore/ForestBookstore/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace ForestBookstore.Controllers
 {
+    using System.Data.Entity;
     using Models;
 
     public class HomeController : Controller
@@ -14,7 +15,9 @@ namespace ForestBookstore.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            this.ViewBag.NewReleases = this.db.Books.Include(b => b.Author).OrderByDescending(b => b.CreatedOn).Take(3);
+            this.ViewBag.BestSellers = this.db.Books.Include(b => b.Author).OrderBy(b => b.CreatedOn).Take(3);
+            return this.View();
         }
     }
 }
