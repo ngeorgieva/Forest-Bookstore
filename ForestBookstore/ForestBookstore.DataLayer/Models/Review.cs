6e047cbddd75116dd29519fcb7cf9067415b8516
ngeorgieva <nat.m.georgieva@gmail.com>
2016-08-17 22:22:37 +0300

    A few bugs fixed

--- ForestBookstore/ForestBookstore.DataLayer/Models/Review.cs
+++ ForestBookstore/ForestBookstore.DataLayer/Models/Review.cs
@@ -20,7 +20,8 @@
         [DataType(DataType.MultilineText)]
         public string Text { get; set; }
 
-        //[Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
 
         public string AuthorId { get; set; }
